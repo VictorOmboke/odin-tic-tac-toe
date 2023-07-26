de1024f78786322f2a763e3effa025f29a8ccbf5
Victor Omboke <victoromboke@gmail.com>
2023-07-26 16:02:35 -0700

    Create objects for the game

---
+++ script.js
@@ -0,0 +1,13 @@
+//Module that houses the logic for the game board.
+const gameBoard = (() => {
+  const board = [];
+  return {};
+})();
+
+//Factory function that creates new players.
+const Player = (name, symbol) => {
+  return { name, symbol };
+};
+
+//Module to control the flow of the game.
+const game = (() => {})();

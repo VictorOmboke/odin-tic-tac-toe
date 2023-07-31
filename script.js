ac01b7d1b4505886a81037f76e5640f54513e19e
Victor Omboke <victoromboke@gmail.com>
2023-07-31 10:34:07 -0700

    Create function to display contents of board arrray

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 //Module that houses the logic for the game board.
 const gameBoard = (() => {
-  const board = [];
-  return {};
+  const board = ["X", "O", "O", "X", "X", "O", "O", "X", "X"];
+
+  const getBoard = () => board;
+
+  const displayBoard = () => {
+    for (let i = 0; i < board.length; i++) {
+      const cells = document.querySelectorAll(".cell");
+      cells[i].textContent = board[i];
+      console.log(board[i]);
+    }
+  };
+
+  return { getBoard, displayBoard };
 })();
+
+gameBoard.displayBoard();
 
 //Factory function that creates new players.
 const Player = (name, symbol) => {

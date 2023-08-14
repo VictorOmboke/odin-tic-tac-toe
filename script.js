ffb4a32aeeb70871012640396236e9283bffb092
Victor Omboke <victoromboke@gmail.com>
2023-08-14 13:47:05 -0700

    Remove radio buttons from HTML and refactor displayBoard and gameFlow modules

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
+//Factory function that creates new players.
+const Player = (name, symbol) => {
+  return { name, symbol };
+};
+
 //Module that houses the logic for the game board.
 const gameBoard = (() => {
   const board = [];
 
+  //Function that displays the game board.
   const displayBoard = () => {
     const cells = document.querySelectorAll(".cell");
 
     cells.forEach((cell, index) => {
       cell.addEventListener("click", () => {
         if (cell.textContent === "") {
-          const currentPlayer = getCurrentPlayer();
-          board[index] = currentPlayer;
-          cell.textContent = currentPlayer;
+          board[index] = gameFlow.getCurrentPlayer();
+          cell.textContent = gameFlow.getCurrentPlayer();
+          gameFlow.PlayerTurn();
           console.log(board);
         }
       });
     });
-
-    const getCurrentPlayer = () => {
-      const radioX = document.getElementById("chooseX");
-      return radioX.checked ? "X" : "O";
-    };
   };
 
   return { displayBoard };
@@ -27,10 +28,22 @@ const gameBoard = (() => {
 
 gameBoard.displayBoard();
 
-//Factory function that creates new players.
-const Player = (name, symbol) => {
-  return { name, symbol };
-};
-
 //Module to control the flow of the game.
-const game = (() => {})();
+const gameFlow = (() => {
+  //Creates new players and sets the current player
+  const player1 = Player("Player 1", "X");
+  const player2 = Player("Player 2", "O");
+  let activePlayer = player1;
+
+  //function that handles the players turns
+  const PlayerTurn = () => {
+    activePlayer = activePlayer === player1 ? player2 : player1;
+  };
+
+  //function to get the current players symbol
+  const getCurrentPlayer = () => {
+    return activePlayer.symbol;
+  };
+
+  return { getCurrentPlayer, PlayerTurn };
+})();
